7ca569e2b07083aaeac2c85c2e60868a83692ed6
Seth Battis <sbattis@groton.org>
2025-04-20 22:30:48 -0400

    fix: register Launch Data repository

--- app/repositories.php
+++ app/repositories.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use App\Domain\AppCredentials\AppCredentialsRepositoryInterface;
+use App\Domain\LTI\LaunchDataRepositoryInterface;
 use App\Infrastructure\Persistence\SecretsAppCredentials;
+use App\Infrastructure\Session\SessionLaunchDataRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
