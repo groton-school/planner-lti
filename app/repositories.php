45363875b7269642af7faac97e8eefa227624e44
Seth Battis <sbattis@groton.org>
2025-04-20 22:32:24 -0400

    feat: store LMS user data in Google Firestore

--- app/repositories.php
+++ app/repositories.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use App\Domain\AppCredentials\AppCredentialsRepositoryInterface;
 use App\Domain\LTI\LaunchDataRepositoryInterface;
+use App\Domain\User\UserRepositoryInterface;
+use App\Infrastructure\Persistence\FirestoreUserRepository;
 use App\Infrastructure\Persistence\SecretsAppCredentials;
 use App\Infrastructure\Session\SessionLaunchDataRepository;
 use DI\ContainerBuilder;
@@ -11,5 +13,6 @@ use DI\ContainerBuilder;
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         AppCredentialsRepositoryInterface::class => DI\autowire(SecretsAppCredentials::class),
+        UserRepositoryInterface::class => DI\autowire(FirestoreUserRepository::class),
     ]);
 };
