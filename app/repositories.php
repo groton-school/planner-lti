a10e410dd619848785e8c7b9626b7397ca089fff
Seth Battis <sbattis@groton.org>
2025-04-20 22:53:27 -0400

    feat: store App Credentials in Google Secret Manager

--- app/repositories.php
+++ app/repositories.php
@@ -2,12 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Domain\AppCredentials\AppCredentialsRepositoryInterface;
 use App\Domain\LTI\LaunchDataRepositoryInterface;
+use App\Infrastructure\Persistence\SecretsAppCredentials;
 use App\Infrastructure\Session\SessionLaunchDataRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         LaunchDataRepositoryInterface::class => DI\autowire(SessionLaunchDataRepository::class),
+        AppCredentialsRepositoryInterface::class => DI\autowire(SecretsAppCredentials::class),
     ]);
 };
