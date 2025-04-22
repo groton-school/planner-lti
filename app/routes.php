df137077c6ac59c5f045182cbd4fb1c64fc2ad07
Seth Battis <sbattis@groton.org>
2025-04-22 09:21:02 -0400

    fix: remove (incomplete) CORS since there will be no cross-origin JS requests

--- app/routes.php
+++ app/routes.php
@@ -10,17 +10,10 @@ use GrotonSchool\Slim\LTI\Actions\LaunchAction;
 use GrotonSchool\Slim\LTI\Actions\LoginAction;
 use GrotonSchool\Slim\LTI\Actions\RegistrationStartAction;
 use Odan\Session\Middleware\SessionStartMiddleware;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $app->options('/{routes:.*}', function (Request $request, Response $response) {
-        // CORS Pre-Flight OPTIONS Request Handler
-        return $response;
-    });
-
     // return an empty string on GAE start/stop requests
     $app->get('/_ah/{action:.*}', EmptyAction::class);
 
