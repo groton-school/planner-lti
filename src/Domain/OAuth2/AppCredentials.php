d6d91af316ab2889fe54818731db46f34df712cf
Seth Battis <sbattis@groton.org>
2025-05-05 12:33:54 -0400

    refactor: remove unused code

--- src/Domain/OAuth2/AppCredentials.php
+++ src/Domain/OAuth2/AppCredentials.php
@@ -6,23 +6,4 @@ class AppCredentials
 {
     public const CANVAS_CLIENT_ID = 'CANVAS_CLIENT_ID';
     public const CANVAS_CLIENT_SECRET = 'CANVAS_CLIENT_SECRET';
-
-    private string $clientId;
-    private string $clientSecret;
-
-    public function __construct(string $clientId, string $clientSecret)
-    {
-        $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
-    }
-
-    public function getClientId()
-    {
-        return $this->clientId;
-    }
-
-    public function getClientSecret()
-    {
-        return $this->clientSecret;
-    }
 }
