20ae05a80b0f3c9dc6bf59418bb29c5b6edffac2
Seth Battis <sbattis@groton.org>
2025-05-06 12:26:45 -0400

    feat: use timeGrid for week and default to week view

--- src/SPA/Calendar.ts
+++ src/SPA/Calendar.ts
@@ -1,17 +1,18 @@
 import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 export function replaceContent(parent: Element, options: CalendarOptions = {}) {
   const calendarElt = document.createElement('div');
   calendarElt.id = 'calendar';
   const calendar = new Calendar(calendarElt, {
-    plugins: [dayGridPlugin, listPlugin],
-    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, listPlugin, timeGridPlugin],
+    initialView: 'timeGridWeek',
     headerToolbar: {
       start: 'today prev,next',
       center: 'title',
-      end: 'dayGridWeek,dayGridMonth,listWeek'
+      end: 'timeGridWeek,dayGridMonth,listWeek'
     },
     ...options
   });
