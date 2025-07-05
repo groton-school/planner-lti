77176c6597dadfbd9801412ab77b4d5b5e4d842e
Seth Battis <sbattis@groton.org>
2025-07-04 22:34:21 -0400

    refactor: correct AbstractAction

--- src/Application/Actions/OAuth2/AbstractOAuth2Action.php
+++ src/Application/Actions/OAuth2/AbstractOAuth2Action.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\OAuth2;
 
-use App\Application\Actions\AbstractAction;
 use App\Application\LoggerTrait;
-use App\Application\LoggingTrait;
 use App\Domain\OAuth2\AppCredentialsRepositoryInterface;
 use App\Domain\OAuth2\OAuth2Trait;
 use App\Domain\LTI\LaunchDataRepositoryInterface;
 use App\Domain\User\UserRepositoryInterface;
 use App\Domain\User\UsersTrait;
 use App\Infrastructure\Session\SessionTrait;
-use GrotonSchool\Slim\Actions\AbstractAction as ActionsAbstractAction;
+use GrotonSchool\Slim\Actions\AbstractAction;
 use Odan\Session\SessionInterface;
 use Psr\Log\LoggerInterface;
 
-abstract class AbstractOAuth2Action extends ActionsAbstractAction
+abstract class AbstractOAuth2Action extends AbstractAction
 {
     use OAuth2Trait;
     use SessionTrait;
