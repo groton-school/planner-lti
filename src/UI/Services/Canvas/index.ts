cafc165d5e5cfb540801e0865d99b8f6aca14e4a
Seth Battis <sbattis@groton.org>
2025-10-07 12:36:54 -0400

    fix: request Canvas API authorization if no valid stored token

--- src/UI/Services/Canvas/index.ts
+++ src/UI/Services/Canvas/index.ts
@@ -1,6 +1,15 @@
-import { Canvas } from '@groton/canvas-api.client.web';
+import {
+  AuthorizationEvent,
+  AuthorizationRequired,
+  Canvas
+} from '@groton/canvas-api.client.web';
+import * as cookie from 'cookie';
+import { render } from 'ejs';
+import * as Activity from '../Activity';
+import * as Bootstrap from '../Bootstrap';
 import * as Courses from './Courses';
 import * as Users from './Users';
+import authorize from './authorize.ejs';
 
 export * as AssignmentGroups from './AssignmentGroups';
 export * as Assignments from './Assignments';
@@ -12,6 +21,22 @@ export * as Users from './Users';
 export const v1 = Canvas.v1;
 
 export async function init() {
+  document.addEventListener(AuthorizationRequired, handleAuthorization);
   Canvas.init();
   await Promise.all([Courses.init(), Users.init()]);
+}
+
+async function handleAuthorization(event: Event) {
+  const { authorize_url } = event as AuthorizationEvent;
+  const { 'planner-session': session } = cookie.parse(document.cookie);
+  Activity.reset();
+  const { modal, elt } = await Bootstrap.Modal.create({
+    title: 'Authorization',
+    body: render(authorize, {
+      authorize_url: authorize_url + `?session=${session}`
+    })
+  });
+  elt.querySelector('#authorize')?.addEventListener('click', () => {
+    modal.hide();
+  });
 }
