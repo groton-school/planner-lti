007aca0c55aea2e360c5c63e96b22e88c25df2f7
Seth Battis <sbattis@groton.org>
2025-10-09 14:12:19 -0400

    feat: move to Bootstrap 5 theming in FullCalendar

--- src/UI/Services/FullCalendar/index.ts
+++ src/UI/Services/FullCalendar/index.ts
@@ -1,3 +1,4 @@
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
@@ -5,11 +6,59 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import './canvaslms.scss';
 import './styles.scss';
 
+export type Options = { selector?: string; options?: CalendarOptions };
+
+const Active = 'fc-button-active';
+
+type ViewStyle = 'grid' | 'list';
+type ViewDuration = 'day' | 'week' | 'month';
+
 let instance: Calendar | undefined = undefined;
 
-const READY = 'calendar.ready';
+export function getInstance() {
+  if (!instance) {
+    throw new Error('FullCalendar not initialized');
+  }
+  return instance;
+}
 
-export type Options = { selector?: string; options?: CalendarOptions };
+const views: Record<ViewStyle, Record<ViewDuration, string>> = {
+  grid: {
+    day: 'timeGridDay',
+    week: 'timeGridWeek',
+    month: 'dayGridMonth'
+  },
+  list: {
+    day: 'listDay',
+    week: 'listWeek',
+    month: 'listMonth'
+  }
+};
+
+let currentDuration: ViewDuration = 'week';
+let currentStyle: ViewStyle = 'grid';
+
+export function changeView(
+  style: ViewStyle,
+  duration: ViewDuration,
+  elt: HTMLElement
+) {
+  currentStyle = style;
+  currentDuration = duration;
+  toggleActiveView(elt);
+  getInstance().changeView(views[currentStyle][currentDuration]);
+}
+
+function toggleActiveView(elt: HTMLElement) {
+  for (const button of elt.closest('.btn-group')?.querySelectorAll('.btn') ||
+    []) {
+    button.classList.remove(Active);
+    button.removeAttribute('disabled');
+  }
+  const button = elt.closest('.btn');
+  button?.classList.add(Active);
+  button?.setAttribute('disabled', 'true');
+}
 
 export function init({ selector = '#calendar', options }: Options) {
   const elt = document.querySelector(selector) as HTMLElement;
@@ -18,23 +67,55 @@ export function init({ selector = '#calendar', options }: Options) {
   }
   elt.innerHTML = '';
   instance = new Calendar(elt, {
-    plugins: [dayGridPlugin, listPlugin, timeGridPlugin],
+    plugins: [dayGridPlugin, listPlugin, timeGridPlugin, bootstrap5Plugin],
+    themeSystem: 'bootstrap5',
     initialView: 'timeGridWeek',
+    eventDisplay: 'block',
+
+    customButtons: {
+      toDay: {
+        text: 'Day',
+        icon: 'calendar-event',
+        click: (event, elt) => changeView(currentStyle, 'day', elt)
+      },
+      toWeek: {
+        text: 'Week',
+        icon: 'calendar-week',
+        click: (event, elt) => changeView(currentStyle, 'week', elt)
+      },
+      toMonth: {
+        text: 'Month',
+        icon: 'calendar3',
+        click: (event, elt) => changeView(currentStyle, 'month', elt)
+      },
+      toGrid: {
+        text: 'Grid',
+        icon: 'grid-fill',
+        click: (event, elt) => changeView('grid', currentDuration, elt)
+      },
+      toList: {
+        text: 'List',
+        icon: 'list-task',
+        click: (event, elt) => changeView('list', currentDuration, elt)
+      }
+    },
+
     headerToolbar: {
       start: 'today prev,next',
       center: 'title',
       // TODO separate duration/presentation options
-      end: 'timeGridWeek,dayGridMonth,listWeek'
+      end: 'toDay,toWeek,toMonth toGrid,toList'
     },
+
     ...options
   });
   instance.render();
-  document.dispatchEvent(new Event(READY));
-}
 
-export function getInstance() {
-  if (!instance) {
-    throw new Error('FullCalendar not initialized');
+  // TODO store user view preferences
+  for (const button of document.querySelectorAll(
+    '.fc-toWeek-button, .fc-toGrid-button'
+  )) {
+    button.classList.add(Active);
+    button.setAttribute('disabled', 'true');
   }
-  return instance;
 }
